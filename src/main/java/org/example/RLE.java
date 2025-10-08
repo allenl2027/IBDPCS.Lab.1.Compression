e0a2d6269c5ae17fad70a653b3710532038c337c
Allen Liang <notallenliang293@gmail.com>
2025-10-08 09:16:32 +0800

    done

--- src/main/java/org/example/RLE.java
+++ src/main/java/org/example/RLE.java
@@ -26,12 +26,52 @@ public class RLE {
     }
 
     /** TODO 1: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to compress a String. Returns the compressed String. */
-    public static String compress(String uncompressed) { return null; }
+    public static String compress(String uncompressed) {
+        String comped="";
+        char prev=uncompressed.charAt(0);
+        int count=1;
+        for(int i=1;i<uncompressed.length();i++){
+            char curr=uncompressed.charAt(i);
+            if(curr==prev) {
+                count++;
+            }
+            else {
+                comped=comped+prev+count;
+                prev=curr;
+                count=1;
+            }
+        }
+        comped=comped+prev+count;
+        return comped;
+    }
 
     /** TODO 2: Given a String (a genome sequence of COVID-19) implement the RLE algorithm that will use RLE to decompress a String. Returns the uncompressed String. */
     public static String decompress(String compressed) {
-        return null;
+        String decomped="";
+        char letter=0;
+        String countStr="";
+        for (int i=0; i<compressed.length();i++) {
+            char c=compressed.charAt(i);
+            if (Character.isLetter(c)) {
+                if (letter!=0 && !countStr.isEmpty()) {
+                    int count=Integer.parseInt(countStr);
+                    for (int k=0;k<count;k++) {
+                        decomped=decomped+letter;
+                    }
+                    countStr="";
+                }
+                letter=c;
+            }
+            else if (Character.isDigit(c)) {
+                countStr += c;
+            }
+        }
+        if (letter!=0 && !countStr.isEmpty()) {
+            int count=Integer.parseInt(countStr);
+            for (int k=0;k<count;k++) {
+                decomped=decomped+letter;
+            }
+        }
+        return decomped;
     }
-
-
 }
